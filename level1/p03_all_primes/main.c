467bd18d069513361834e64d65f60831b19498a2
xulingbumei <2056786391@qq.com>
2023-10-30 19:41:36 +0800

    调整了作业、readme、cmakelist

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,22 @@
-#include <printf.h>
+#include <stdio.h>
 
 int main() {
-    printf("hello world!\n");
-    return 0;
+    int i,num,flag;
+    num=2;
+    flag=1;
+    while (num<=999){
+        for(i=2;i<num;i++){
+            if(num%i==0){
+                flag=0;
+                break;}
+        }
+        if(flag==1){
+            printf("%d\n" ,num);}
+        else{
+            flag=1;}
+        num++ ;
+    }
+return 0;
 }
+
+
