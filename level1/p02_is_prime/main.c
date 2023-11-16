28deff14b4a6b77a4244c09a433dfa7bb81c4a69
xulingbumei <2056786391@qq.com>
2023-11-16 20:16:55 +0800

    调整了作业、readme、cmakelist

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,14 @@
-#include <printf.h>
+#include <stdio.h >
 
 int main() {
-    printf("hello world!\n");
+    int n,i;
+    scanf("%d", &n);
+    for (i = 2; i < n; i++) {
+        if (n % i == 0) {
+            printf("no\n");
+            return 0;
+        }
+    }
+    printf("yes\n");
     return 0;
 }
